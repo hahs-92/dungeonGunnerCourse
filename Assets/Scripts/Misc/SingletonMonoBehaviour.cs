c75d9242b50b064806fcb1aaab2727efed70b611
unknown <alexandre_2420@outlook.com>
2023-02-18 12:22:53 -0500

    section 8: Create Singleton Abstract Class

---
+++ Assets/Scripts/Misc/SingletonMonoBehaviour.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+        }
+        else
+        {
+            // se debe destruir porque es una instancia static, solo puede haber una instancia
+            Destroy(gameObject);
+        }
+    }
+}
