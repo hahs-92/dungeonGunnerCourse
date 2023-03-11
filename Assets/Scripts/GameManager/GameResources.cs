291f3eeaa01826d715f5d6e49a2991f86fbcb8ef
unknown <alexandre_2420@outlook.com>
2023-03-11 15:39:22 -0500

    section 9: Initial Player Classes

--- Assets/Scripts/GameManager/GameResources.cs
+++ Assets/Scripts/GameManager/GameResources.cs
@@ -25,8 +25,17 @@ public class GameResources : MonoBehaviour
     #region Tooltip
     [Tooltip("Populate with the dungeon RoomNodeTypeListSO")]
     #endregion
-
     public RoomNodeTypeListSO roomNodeTypeList;
+
+    #region Header PLAYER
+    [Space(10)]
+    [Header("PLAYER")]
+    #endregion Header PLAYER
+    #region Tooltip
+    [Tooltip("The current player scriptable object - this is used to reference the current player between scenes")]
+    #endregion Tooltip
+    public CurrentPlayerSO currentPlayer;
+
 
     #region Header MATERIALS
     [Space(10)]
