dab0a668703e201de8e5290004c1d9bcbf578135
unknown <alexandre_2420@outlook.com>
2023-04-07 15:33:36 -0500

    section 10: 53. Cinemachine target group

--- Assets/Scripts/Misc/CinemachineTarget.cs
+++ Assets/Scripts/Misc/CinemachineTarget.cs
@@ -8,6 +8,11 @@ public class CinemachineTarget : MonoBehaviour
 {
     private CinemachineTargetGroup cinemachineTargetGroup;
 
+    #region Tooltip
+    [Tooltip("Populate with the CursorTarget gameobject")]
+    #endregion Tooltip
+    [SerializeField] private Transform cursorTarget;
+
     private void Awake()
     {
         // Load components
@@ -20,6 +25,11 @@ public class CinemachineTarget : MonoBehaviour
         SetCinemachineTargetGroup();
     }
 
+    private void Update()
+    {
+        cursorTarget.position = HelperUtilities.GetMouseWorldPosition();
+    }
+
     /// <summary>
     /// Set the cinemachine camera target group.
     /// </summary>
@@ -28,10 +38,12 @@ public class CinemachineTarget : MonoBehaviour
         // este target se agrega a la lista del chinemachicTargetGroup al iniciar la partida
         // de esta manera la camara se centra en el player
 
-        // Create target group for cinemachine for the cinemachine camera to follow 
-        CinemachineTargetGroup.Target cinemachineGroupTarget_player = new CinemachineTargetGroup.Target { weight = 1f, radius = 1f, target = GameManager.Instance.GetPlayer().transform };
+        // Create target group for cinemachine for the cinemachine camera to follow - group will include the player and screen cursor
+        CinemachineTargetGroup.Target cinemachineGroupTarget_player = new CinemachineTargetGroup.Target { weight = 1f, radius = 2.5f, target = GameManager.Instance.GetPlayer().transform };
 
-        CinemachineTargetGroup.Target[] cinemachineTargetArray = new CinemachineTargetGroup.Target[] { cinemachineGroupTarget_player };
+        CinemachineTargetGroup.Target cinemachineGroupTarget_cursor = new CinemachineTargetGroup.Target { weight = 1f, radius = 1f, target = cursorTarget };
+
+        CinemachineTargetGroup.Target[] cinemachineTargetArray = new CinemachineTargetGroup.Target[] { cinemachineGroupTarget_player, cinemachineGroupTarget_cursor };
 
         cinemachineTargetGroup.m_Targets = cinemachineTargetArray;
 
