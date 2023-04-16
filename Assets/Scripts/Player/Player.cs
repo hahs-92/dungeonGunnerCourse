0b78caeb3fe65fcdc098dd1b4a5fa6f74a3d2d68
unknown <alexandre_2420@outlook.com>
2023-04-16 12:47:10 -0500

    section 13: 68. Player Starting Weapon

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -15,6 +15,8 @@ using UnityEngine.Rendering;
 [RequireComponent(typeof(Idle))]
 [RequireComponent(typeof(AimWeaponEvent))]
 [RequireComponent(typeof(AimWeapon))]
+[RequireComponent(typeof(SetActiveWeaponEvent))]
+[RequireComponent(typeof(ActiveWeapon))]
 [RequireComponent(typeof(AnimatePlayer))]
 [RequireComponent(typeof(SortingGroup))]
 [RequireComponent(typeof(SpriteRenderer))]
@@ -34,8 +36,12 @@ public class Player : MonoBehaviour
     [HideInInspector] public MovementToPositionEvent movementToPositionEvent;
     [HideInInspector] public IdleEvent idleEvent;
     [HideInInspector] public AimWeaponEvent aimWeaponEvent;
+    [HideInInspector] public SetActiveWeaponEvent setActiveWeaponEvent;
+    [HideInInspector] public ActiveWeapon activeWeapon;
     [HideInInspector] public SpriteRenderer spriteRenderer;
     [HideInInspector] public Animator animator;
+
+    public List<Weapon> weaponList = new List<Weapon>();
 
     private void Awake()
     {
@@ -45,6 +51,8 @@ public class Player : MonoBehaviour
         movementToPositionEvent = GetComponent<MovementToPositionEvent>();
         idleEvent = GetComponent<IdleEvent>();
         aimWeaponEvent = GetComponent<AimWeaponEvent>();
+        setActiveWeaponEvent = GetComponent<SetActiveWeaponEvent>();
+        activeWeapon = GetComponent<ActiveWeapon>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
     }
@@ -57,8 +65,27 @@ public class Player : MonoBehaviour
     {
         this.playerDetails = playerDetails;
 
+        //Create player starting weapons
+        CreatePlayerStartingWeapons();
+
         // Set player starting health
         SetPlayerHealth();
+    }
+
+    /// <summary>
+    /// Set the player starting weapon
+    /// </summary>
+    private void CreatePlayerStartingWeapons()
+    {
+        // Clear list
+        weaponList.Clear();
+
+        // Populate weapon list from starting weapons
+        foreach (WeaponDetailsSO weaponDetails in playerDetails.startingWeaponList)
+        {
+            // Add weapon to player
+            AddWeaponToPlayer(weaponDetails);
+        }
     }
 
     /// <summary>
@@ -67,5 +94,31 @@ public class Player : MonoBehaviour
     private void SetPlayerHealth()
     {
         health.SetStartingHealth(playerDetails.playerHealthAmount);
+    }
+
+
+    /// <summary>
+    /// Add a weapon to the player weapon dictionary
+    /// </summary>
+    public Weapon AddWeaponToPlayer(WeaponDetailsSO weaponDetails)
+    {
+        Weapon weapon = new Weapon() { 
+            weaponDetails = weaponDetails,
+            weaponReloadTimer = 0f, 
+            weaponClipRemainingAmmo = weaponDetails.weaponClipAmmoCapacity,
+            weaponRemainingAmmo = weaponDetails.weaponAmmoCapacity,
+            isWeaponReloading = false 
+        };
+
+        // Add the weapon to the list
+        weaponList.Add(weapon);
+
+        // Set weapon position in list
+        weapon.weaponListPosition = weaponList.Count;
+
+        // Set the added weapon as active
+        setActiveWeaponEvent.CallSetActiveWeaponEvent(weapon);
+
+        return weapon; 
     }
 }
--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -1,7 +1,8 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Player))]
+[DisallowMultipleComponent]
 public class PlayerControl : MonoBehaviour
 {
     #region Tooltip
@@ -9,12 +10,8 @@ public class PlayerControl : MonoBehaviour
     #endregion Tooltip
     [SerializeField] private MovementDetailsSO movementDetails;
 
-    #region Tooltip
-    [Tooltip("The player WeaponShootPosition gameobject in the hieracrchy")]
-    #endregion Tooltip
-    [SerializeField] private Transform weaponShootPosition;
-
     private Player player;
+    private int currentWeaponIndex = 1;
     private float moveSpeed;
     private Coroutine playerRollCoroutine;
     private WaitForFixedUpdate waitForFixedUpdate;
@@ -32,6 +29,9 @@ public class PlayerControl : MonoBehaviour
     {
         // Create waitforfixed update for use in coroutine
         waitForFixedUpdate = new WaitForFixedUpdate();
+
+        // Set Starting Weapon
+        SetStartingWeapon();
 
         // Set player animation speed
         SetPlayerAnimationSpeed();
@@ -51,7 +51,24 @@ public class PlayerControl : MonoBehaviour
 
         // Player roll cooldown timer
         PlayerRollCooldownTimer();
+    }
 
+    /// <summary>
+    /// Set the player starting weapon
+    /// </summary>
+    private void SetStartingWeapon()
+    {
+        int index = 1;
+
+        foreach (Weapon weapon in player.weaponList)
+        {
+            if (weapon.weaponDetails == player.playerDetails.startingWeapon)
+            {
+                SetWeaponByIndex(index);
+                break;
+            }
+            index++;
+        }
     }
 
     /// <summary>
@@ -167,7 +184,7 @@ public class PlayerControl : MonoBehaviour
         Vector3 mouseWorldPosition = HelperUtilities.GetMouseWorldPosition();
 
         // Calculate direction vector of mouse cursor from weapon shoot position
-        weaponDirection = (mouseWorldPosition - weaponShootPosition.position);
+        weaponDirection = (mouseWorldPosition - player.activeWeapon.GetShootPosition());
 
         // Calculate direction vector of mouse cursor from player transform position
         Vector3 playerDirection = (mouseWorldPosition - transform.position);
@@ -183,6 +200,15 @@ public class PlayerControl : MonoBehaviour
 
         // Trigger weapon aim event
         player.aimWeaponEvent.CallAimWeaponEvent(playerAimDirection, playerAngleDegrees, weaponAngleDegrees, weaponDirection);
+    }
+
+    private void SetWeaponByIndex(int weaponIndex)
+    {
+        if (weaponIndex - 1 < player.weaponList.Count)
+        {
+            currentWeaponIndex = weaponIndex;
+            player.setActiveWeaponEvent.CallSetActiveWeaponEvent(player.weaponList[weaponIndex - 1]);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
