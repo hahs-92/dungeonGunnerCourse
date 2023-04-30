46a9e7ed6ee038862e9fe00682884b1683c28e16
unknown <alexandre_2420@outlook.com>
2023-04-30 17:10:38 -0500

    section 16: 102: Ammo hit effects for others weapons

--- Assets/Scripts/Weapons/Ammo/Ammo.cs
+++ Assets/Scripts/Weapons/Ammo/Ammo.cs
@@ -56,6 +56,9 @@ public class Ammo : MonoBehaviour, IFireable
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Show ammo hit effect
+        AmmoHitEffect();
+
         DisableAmmo();
     }
 
@@ -168,6 +171,24 @@ public class Ammo : MonoBehaviour, IFireable
     private void DisableAmmo()
     {
         gameObject.SetActive(false);
+    }
+
+    private void AmmoHitEffect()
+    {
+        // Process if a hit effect has been specified
+        if (ammoDetails.ammoHitEffect != null && ammoDetails.ammoHitEffect.ammoHitEffectPrefab != null)
+        {
+            // Get ammo hit effect gameobject from the pool (with particle system component)
+            AmmoHitEffect ammoHitEffect = (AmmoHitEffect)PoolManager.Instance
+                .ReuseComponent(ammoDetails.ammoHitEffect.ammoHitEffectPrefab, transform.position, Quaternion.identity);
+
+            // Set Hit Effect
+            ammoHitEffect.SetHitEffect(ammoDetails.ammoHitEffect);
+
+            // Set gameobject active (the particle system is set to automatically disable the
+            // gameobject once finished)
+            ammoHitEffect.gameObject.SetActive(true);
+        }
     }
 
     public void SetAmmoMaterial(Material material)
