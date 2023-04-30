c09a1d98ae810db3a74b2c5d27cafe8397d25a86
unknown <alexandre_2420@outlook.com>
2023-04-30 11:26:55 -0500

    section 16: 95. Add Smoke Shoot Effect to pistol

--- Assets/Scripts/Weapons/Weapons/WeaponDetailsSO.cs
+++ Assets/Scripts/Weapons/Weapons/WeaponDetailsSO.cs
@@ -36,6 +36,11 @@ public class WeaponDetailsSO : ScriptableObject
     public AmmoDetailsSO weaponCurrentAmmo;
 
     #region Tooltip
+    [Tooltip("Weapon shoot effect SO - contains particle effecct parameters to be used in conjunction with the weaponShootEffectPrefab ")]
+    #endregion Tooltip
+    public WeaponShootEffectSO weaponShootEffect;
+
+    #region Tooltip
     [Tooltip("The firing sound effect SO for the weapon")]
     #endregion Tooltip
     public SoundEffectSO weaponFiringSoundEffect;
