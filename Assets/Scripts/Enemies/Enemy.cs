aaba11966edd2fab47cc45cd89d0de82aa8c60c9
unknown <alexandre_2420@outlook.com>
2023-05-01 11:57:55 -0500

    section 17: 107. Enemy animator controller & clips

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -4,6 +4,7 @@ using UnityEngine.Rendering;
 #region REQUIRE COMPONENTS
 [RequireComponent(typeof(SortingGroup))]
 [RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(CircleCollider2D))]
 [RequireComponent(typeof(PolygonCollider2D))]
@@ -16,6 +17,7 @@ public class Enemy : MonoBehaviour
     private PolygonCollider2D polygonCollider2D;
     [HideInInspector] public SpriteRenderer[] spriteRendererArray;
     [HideInInspector] public EnemyDetailsSO enemyDetails;
+    [HideInInspector] public Animator animator;
 
     private void Awake()
     {
@@ -23,5 +25,6 @@ public class Enemy : MonoBehaviour
         circleCollider2D = GetComponent<CircleCollider2D>();
         polygonCollider2D = GetComponent<PolygonCollider2D>();
         spriteRendererArray = GetComponentsInChildren<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 }
