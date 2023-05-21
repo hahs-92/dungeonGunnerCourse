9b26e7ab01e2fe5110c8aebe6a6b73fb287e108d
unknown <alexandre_2420@outlook.com>
2023-05-21 16:11:33 -0500

    section 30: 155. Defining music for rooms

--- Assets/Scripts/Dungeon/RoomTemplateSO.cs
+++ Assets/Scripts/Dungeon/RoomTemplateSO.cs
@@ -25,27 +25,34 @@ public class RoomTemplateSO : ScriptableObject
     [HideInInspector] public GameObject previousPrefab; // this is used to regenerate the guid if the so is copied and the prefab is changed
 
 
-    #region Header ROOM CONFIGURATION
+    #region Header ROOM MUSIC
 
     [Space(10)]
-    [Header("ROOM CONFIGURATION")]
-
-    #endregion Header ROOM CONFIGURATION
+    [Header("ROOM MUSIC")]
+    #endregion Header ROOM MUSIC
 
     #region Tooltip
-
-    [Tooltip("The room node type SO. The room node types correspond to the room nodes used in the room node graph.  The exceptions being with corridors.  In the room node graph there is just one corridor type 'Corridor'.  For the room templates there are 2 corridor node types - CorridorNS and CorridorEW.")]
-
+    [Tooltip("The battle music SO when the room hasn't been cleared of enemies")]
     #endregion Tooltip
+    public MusicTrackSO battleMusic;
 
+    #region Tooltip
+    [Tooltip("Ambient music SO for when the room has been cleared of enemies")]
+    #endregion Tooltip
+    public MusicTrackSO ambientMusic;
+
+    #region Header ROOM CONFIGURATION
+    [Space(10)]
+    [Header("ROOM CONFIGURATION")]
+    #endregion Header ROOM CONFIGURATION
+    #region Tooltip
+    [Tooltip("The room node type SO. The room node types correspond to the room nodes used in the room node graph.  The exceptions being with corridors.  In the room node graph there is just one corridor type 'Corridor'.  For the room templates there are 2 corridor node types - CorridorNS and CorridorEW.")]
+    #endregion Tooltip
     public RoomNodeTypeSO roomNodeType;
 
     #region Tooltip
-
     [Tooltip("If you imagine a rectangle around the room tilemap that just completely encloses it, the room lower bounds represent the bottom left corner of that rectangle. This should be determined from the tilemap for the room (using the coordinate brush pointer to get the tilemap grid position for that bottom left corner (Note: this is the local tilemap position and NOT world position")]
-
     #endregion Tooltip
-
     public Vector2Int lowerBounds;
 
     #region Tooltip
@@ -113,8 +120,9 @@ public class RoomTemplateSO : ScriptableObject
             EditorUtility.SetDirty(this);
         }
         HelperUtilities.ValidateCheckNullValue(this, nameof(prefab), prefab);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(battleMusic), battleMusic);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(ambientMusic), ambientMusic);
         HelperUtilities.ValidateCheckNullValue(this, nameof(roomNodeType), roomNodeType);
-
         HelperUtilities.ValidateCheckEnumerableValues(this, nameof(doorwayList), doorwayList);
 
         // Check enemies and room spawn parameters for levels
